056984f9880d3aee2375937bc78488a27d4c5b86
muhammedarshadtp <arshadtp89@gmail.com>
2026-01-01 00:05:42 +0530

    creata a new file and display the form

---
+++ src/components/Basic.jsx
@@ -0,0 +1,77 @@
+
+import React from 'react';
+import { Formik } from 'formik';
+
+const Basic = () => (
+  <div className='formik'>
+    <h1 className='h1'>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form className="form" onSubmit={handleSubmit}>
+  <div className="form-group">
+    <label>Email</label>
+    <input
+      type="email"
+      name="email"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.email}
+    />
+    {errors.email && touched.email && (
+      <span className="error">{errors.email}</span>
+    )}
+  </div>
+
+  <div className="form-group">
+    <label>Password</label>
+    <input
+      type="password"
+      name="password"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.password}
+    />
+    {errors.password && touched.password && (
+      <span className="error">{errors.password}</span>
+    )}
+  </div>
+
+  <button type="submit" disabled={isSubmitting}>
+    {isSubmitting ? "Submitting..." : "Submit"}
+  </button>
+</form>
+
+      )}
+    </Formik>
+  </div>
+);
+
+export default Basic;
